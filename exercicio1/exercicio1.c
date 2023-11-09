317b486e8c8f49c908e5edad8521885f985be6ca
Pedro Souza <mypedrosouza@gmail.com>
2023-11-09 12:04:15 -0300

    adicionado pastas para cada exercício

---
+++ exercicio1/exercicio1.c
@@ -0,0 +1,24 @@
+// Fundamentos da programação de computadores: algoritmos, Pascal, C/C++ e Java. 3. ed.
+#include <stdio.h>
+
+int main()
+{
+    // Declarações
+    int n1, n2, r;
+    
+    // Pede e recebe número 1
+    printf("Digite um número: ");
+    scanf("%d", &n1);
+    
+    // Pede e recebe número 2
+    printf("Digite outro número: ");
+    scanf("%d", &n2);
+    
+    // Subtração (Processamento)
+    r = n1 - n2;
+    
+    // Mostra o resultado 
+    printf("A subtração do número %d pelo número %d é %d", n1, n2, r);
+    
+    return 1;
+}
---
+++ exercicio3/exercicio3.c
@@ -0,0 +1,25 @@
+// Fundamentos da programação de computadores: algoritmos, Pascal, C/C++ e Java. 3. ed.
+#include <stdio.h>
+
+int main()
+{
+    // Declarações
+    float n1, n2;
+    float r;
+    
+    // Pede e recebe número 1
+    printf("Digite um número: ");
+    scanf("%f", &n1);
+    
+    // Pede e recebe número 2
+    printf("Digite outro número: ");
+    scanf("%f", &n2);
+    
+    // Divisão (Processamento)
+    r = n1 / n2;
+    
+    // Mostra o resultado 
+    printf("A divisão do número %f pelo número %f é %f", n1, n2, r);
+
+    return 1;
+}
